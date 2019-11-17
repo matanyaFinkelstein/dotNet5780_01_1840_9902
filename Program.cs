6c01951d4bbe6fd4fa37980f90b31d43fae8241c
matanya finkelstein <matanya54@gmail.com>
2019-11-17 15:04:01 +0200

    a

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5780_01_1840_9902
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
