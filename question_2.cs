4a3955ee8f4d694e163e55da3ccfd8994ec8326a
matanya finkelstein <matanya54@gmail.com>
2019-11-18 18:59:13 +0200

    case 0 finished

--- question_2.cs
+++ question_2.cs
@@ -8,6 +8,7 @@ namespace dotNet5780_01_1840_9902_2
 {
     class question_2
     {
+
         public static int Main(string[] args)
         {
             bool[,] host;
@@ -24,59 +25,53 @@ namespace dotNet5780_01_1840_9902_2
             switch (choice)
             {
                 case 0:
-
-<<<<<<< HEAD
-
-=======
                     Console.WriteLine("please enter the date of the visit");
                     string date = Console.ReadLine();
-                    String[] s = date.Split(new char[] { '.' ,'/' });
+                    String[] s = date.Split(new char[] { '.', '/' });
                     int day = Convert.ToInt32(s[0]);
                     int month = Convert.ToInt32(s[1]);
+                    day--; //because the array start from 0 so this is the date minus 1
+                    month--; //because the array start from 0 so this is the date minus 1
                     Console.WriteLine("please enter the duration of the visit");
                     int duration = Convert.ToInt32(Console.ReadLine());
-                    if (host[month, day] == true)
+
+                    bool flag = true;
+                    for (int i = 0; i < duration; i++)
                     {
-                        if (host[month, day + 1] == true)
+                        int days = day + i;
+                        int months = month;
+                        if (days >= 31)
+                        {
+                            months += days / 31;
+                            days = days % 31;
+                        }
+                        if (host[month, day] == true)
+                        {
+                            flag = false;
                             break;
-
-                    }
-                    for (int i = 1; i < duration; i++)
-                    {
-                        if (host[month, day + i] == true)
-                        {
-                            if (host[month, day + i - 1] == true)
-                            {
-                                add
-                            }
                         }
                     }
-
->>>>>>> 8899002002d17eecd4abc47043693ba3c93c0923
+                    if (flag)
+                    {
+                        for (int i = 0; i < duration - 1; i++)
+                        {
+                            int days = day + i;
+                            int months = month;
+                            if (days >= 31)
+                            {
+                                months += days / 31;
+                                days = days % 31;
+                            }
+                            host[month, day] = true;
+                        }
+                        Console.WriteLine("The request has been approved");
+                    }
+                    else
+                        Console.WriteLine("The request has been rejected");
                     break;
-
                 case 1:
-                    bool flag = false, firstDay = false, lastDay = false;
 
-                    for (int i = 0; i < 12; i++)
-                    {
-                        for (int j = 0; j < 31; j++)
-                        {
-                            if (host[i, j])
-                            {
-                                if (!firstDay)
-                                {
-                                    firstDay = true;
-                                    Console.WriteLine("{0}.{1}", j, i);
-                                }
-                            }
-                            else if (firstDay)
-                            {
-                                if (host[i, j - 1])
-                            }
-                        }
-                        break;
-
+                    break;
                 case 2:
                     break;
 
@@ -85,9 +80,6 @@ namespace dotNet5780_01_1840_9902_2
                 default:
                     break;
             }
-
-
-
             return 0;
         }
     }
